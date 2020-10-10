be435d091861678ab68cf63e1284165717340399
Cristi Cismas <cristicismas99@gmail.com>
2020-10-10 12:28:03 +0000

    Fix some formatting issues in serial.rs

--- src/serial.rs
+++ src/serial.rs
@@ -37,6 +37,7 @@ macro_rules! serial_print {
 macro_rules! serial_println {
     () => ($crate::serial_print!("\n"));
     ($fmt:expr) => ($crate::serial_print!(concat!($fmt, "\n")));
-    ($fmt:expr, $($arg:tt)*) => ($crate::serial_print!(
-        concat!($fmt, "\n"), $($arg)*));
+    ($fmt:expr, $($arg:tt)*) => (
+        $crate::serial_print!(concat!($fmt, "\n"), $($arg)*)
+    );
 }
