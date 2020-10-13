38f07cf947d904f5ecfaa478b2d3553c5ca76b3a
Cristi Cismas <cristicismas99@gmail.com>
2020-10-13 18:00:57 +0000

    Implement a better executer and async keyboard support

--- src/lib.rs
+++ src/lib.rs
@@ -5,18 +5,19 @@
 #![reexport_test_harness_main = "test_main"]
 #![feature(abi_x86_interrupt)]
 #![feature(alloc_error_handler)]
+#![feature(wake_trait)]
 
 use core::panic::PanicInfo;
 
 extern crate alloc;
 
+pub mod allocator;
 pub mod gdt;
 pub mod interrupts;
 pub mod memory;
 pub mod serial;
-pub mod vga_buffer;
-pub mod allocator;
 pub mod task;
+pub mod vga_buffer;
 
 #[cfg(test)]
 use bootloader::{entry_point, BootInfo};
--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@
 use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
 use resox::println;
-use resox::task::{Task, simple_executor::SimpleExecutor};
+use resox::task::{keyboard, executor::Executor, Task};
 
 entry_point!(kernel_main);
 
@@ -27,24 +27,22 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
 
     println!("Hello World{}", "!");
     resox::init();
-    
+
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
     let mut mapper = unsafe { memory::init(phys_mem_offset) };
     let mut frame_allocator = unsafe { BootInfoFrameAllocator::init(&boot_info.memory_map) };
 
     allocator::init_heap(&mut mapper, &mut frame_allocator).expect("heap initialization failed");
 
-    let mut executor = SimpleExecutor::new();
-    executor.spawn(Task::new(example_task()));
-    executor.run();
-
     #[cfg(test)]
     test_main();
 
-    resox::hlt_loop();
+    let mut executor = Executor::new();
+    executor.spawn(Task::new(example_task()));
+    executor.spawn(Task::new(keyboard::print_keypresses()));
+    executor.run();
 }
 
-/// This function is called on panic.
 #[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
